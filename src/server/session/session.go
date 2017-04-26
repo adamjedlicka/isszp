32d209e62c16f0f43dec38571ee68e3d0b9e56b3
Adam Jedlička <adajedlicka@gmail.com>
2017-04-26 12:52:37 +0200

    session - pridana dokumentace

--- src/server/session/session.go
+++ src/server/session/session.go
@@ -1,3 +1,5 @@
+// Package session contains implementation of session handling
+// and some useful functions that will help you to correctly a safely extract certain data from sessions
 package session
 
 import (
@@ -6,10 +8,14 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+// Login is cookie name used to store login token
 const Login = "LOGIN"
 
+// Store is a cookie store that is used to obtain user cookies based on its cookie name
 var Store = sessions.NewCookieStore([]byte("something-very-secret"))
 
+// IsLoggedIn returns true if user that sent request r is lgged int.
+// If not or no session/cookie exists IsLoggedIn returns false
 func IsLoggedIn(r *http.Request) bool {
 	s, err := Store.Get(r, Login)
 	if err != nil {
@@ -24,6 +30,8 @@ func IsLoggedIn(r *http.Request) bool {
 	return isLoggedIn
 }
 
+// GetUserName returns user name of currently logged user.
+// If no user is logged in GetUserName returns empty string ""
 func GetUserName(r *http.Request) string {
 	s, err := Store.Get(r, Login)
 	if err != nil {
