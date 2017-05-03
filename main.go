1f520cfe3ed8aaa1965d09366e6659ea43cf912e
Adam Jedlička <adajedlicka@gmail.com>
2017-05-03 11:50:26 +0000

    Pridan config.json

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"flag"
 
+	"log"
+
+	"encoding/json"
+
+	"io/ioutil"
+
 	"gitlab.fit.cvut.cz/isszp/isszp/cmd/install"
 	"gitlab.fit.cvut.cz/isszp/isszp/src/model/db"
 	"gitlab.fit.cvut.cz/isszp/isszp/src/server"
 )
 
+type Config struct {
+	Server   server.Config
+	Database db.Config
+}
+
 func main() {
 	flag.Parse()
 
+	bytes, err := ioutil.ReadFile("./config/config.json")
+	if err != nil {
+		log.Fatal("Missing config file in ./config/ directory!")
+	}
+
+	cfg := Config{}
+	if err := json.Unmarshal(bytes, &cfg); err != nil {
+		panic(err)
+	}
+
 	if flag.Arg(0) == "install" {
-		install.Init()
+		install.InstallDatabase(cfg.Database)
 		return
 	}
 
+	db.Configure(cfg.Database)
 	db.Init()
+
+	server.Configure(cfg.Server)
 	server.Run()
 }
--- src/model/db/database.go
+++ src/model/db/database.go
@@ -2,20 +2,31 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
+type Config struct {
+	User     string
+	Password string
+	Database string
+}
+
 var (
+	cfg Config
+
 	db *gorm.DB
 	sq *sql.DB
 )
 
+func Configure(config Config) { cfg = config }
+
 func Init() {
 	var err error
-	db, err = gorm.Open("mysql", "root:@/isszp")
+	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@/%s", cfg.User, cfg.Password, cfg.Database))
 	if err != nil {
 		panic(err)
 	}
